78f2917fd74e5ce92f2e6fe2110d7792090832cf
Kinnalover <kinnalover@gmail.com>
2025-07-09 14:41:26 +0500

    sidebar added

--- src/lib/flow/Flow.svelte
+++ src/lib/flow/Flow.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
-  import { SvelteFlow, Controls, type Node, type Edge } from '@xyflow/svelte';
+  import { SvelteFlow, Controls, type Node, type Edge, useSvelteFlow } from '@xyflow/svelte';
  
   import '@xyflow/svelte/dist/style.css';
  
   import { initialNodes, initialEdges } from './nodes-and-edges';
   import TurboNode from './TurboNode.svelte';
   import TurboEdge from './TurboEdge.svelte';
- 
+  import { useDnD } from './DnDProvider.svelte';
+  import Sidebar from './Sidebar.svelte';
   let nodes = $state.raw<Node[]>(initialNodes);
   let edges = $state.raw<Edge[]>(initialEdges);
  
+
+  
   const nodeTypes = {
     turbo: TurboNode,
   };
@@ -22,11 +25,48 @@
     type: 'turbo',
     markerEnd: 'edge-circle',
   };
+
+
+  const { screenToFlowPosition } = useSvelteFlow();
+ 
+  const type = useDnD();
+ 
+  const onDragOver = (event: DragEvent) => {
+    event.preventDefault();
+ 
+    if (event.dataTransfer) {
+      event.dataTransfer.dropEffect = 'move';
+    }
+  };
+ 
+  const onDrop = (event: DragEvent) => {
+    event.preventDefault();
+ 
+    if (!type.current) {
+      return;
+    }
+ 
+    const position = screenToFlowPosition({
+      x: event.clientX,
+      y: event.clientY,
+    });
+ 
+    const newNode = {
+      id: `${Math.random()}`,
+      type: type.current,
+      position,
+      data: { label: `${type.current} node` },
+      origin: [0.5, 0.0],
+    } satisfies Node;
+ 
+    nodes = [...nodes, newNode];
+  };
 </script>
  
-<div style:width="100vw" style:height="100vh">
-<SvelteFlow bind:nodes {nodeTypes} bind:edges {edgeTypes} {defaultEdgeOptions} fitView>
+<main>
+<SvelteFlow bind:nodes {nodeTypes} bind:edges {edgeTypes} {defaultEdgeOptions} fitView ondragover={onDragOver} ondrop={onDrop}>
   <Controls showLock={false} />
+  
   <svg>
     <defs>
       <linearGradient id="edge-gradient">
@@ -48,4 +88,12 @@
     </defs>
   </svg>
 </SvelteFlow>
-</div>
+<Sidebar />
+</main>
+<style>
+  main {
+    height: 100vh;
+    display: flex;
+    flex-direction: column-reverse;
+  }
+</style>
