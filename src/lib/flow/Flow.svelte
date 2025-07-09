d5ef1b7d68f8ec926b95ad61d09725c06d9a7b7b
Kinnalover <kinnalover@gmail.com>
2025-07-09 12:38:35 +0500

    turbo

--- src/lib/flow/Flow.svelte
+++ src/lib/flow/Flow.svelte
@@ -1,104 +1,51 @@
 <script lang="ts">
-  import {
-    SvelteFlow,
-    Controls,
-    Background,
-    BackgroundVariant,
-    MiniMap,
-    useSvelteFlow,
-    type Node,
-  } from '@xyflow/svelte';
- 
-  import { useDnD } from './DnDProvider.svelte';
-  import Sidebar from './Sidebar.svelte';
+  import { SvelteFlow, Controls, type Node, type Edge } from '@xyflow/svelte';
  
   import '@xyflow/svelte/dist/style.css';
  
-  let nodes = $state.raw([
-    {
-      id: '1',
-      type: 'input',
-      data: { label: 'Input Node' },
-      position: { x: 150, y: 5 },
-    },
-    {
-      id: '2',
-      type: 'default',
-      data: { label: 'Default Node' },
-      position: { x: 0, y: 150 },
-    },
-    {
-      id: '3',
-      type: 'output',
-      data: { label: 'Output Node' },
-      position: { x: 300, y: 150 },
-    },
-  ]);
+  import { initialNodes, initialEdges } from './nodes-and-edges';
+  import TurboNode from './TurboNode.svelte';
+  import TurboEdge from './TurboEdge.svelte';
  
-  let edges = $state.raw([
-    {
-      id: '1-2',
-      type: 'default',
-      source: '1',
-      target: '2',
-    },
-    {
-      id: '1-3',
-      type: 'smoothstep',
-      source: '1',
-      target: '3',
-    },
-  ]);
+  let nodes = $state.raw<Node[]>(initialNodes);
+  let edges = $state.raw<Edge[]>(initialEdges);
  
-  const { screenToFlowPosition } = useSvelteFlow();
- 
-  const type = useDnD();
- 
-  const onDragOver = (event: DragEvent) => {
-    event.preventDefault();
- 
-    if (event.dataTransfer) {
-      event.dataTransfer.dropEffect = 'move';
-    }
+  const nodeTypes = {
+    turbo: TurboNode,
   };
  
-  const onDrop = (event: DragEvent) => {
-    event.preventDefault();
+  const edgeTypes = {
+    turbo: TurboEdge,
+  };
  
-    if (!type.current) {
-      return;
-    }
- 
-    const position = screenToFlowPosition({
-      x: event.clientX,
-      y: event.clientY,
-    });
- 
-    const newNode = {
-      id: `${Math.random()}`,
-      type: type.current,
-      position,
-      data: { label: `${type.current} node` },
-      origin: [0.5, 0.0],
-    } satisfies Node;
- 
-    nodes = [...nodes, newNode];
+  const defaultEdgeOptions = {
+    type: 'turbo',
+    markerEnd: 'edge-circle',
   };
 </script>
  
-<main>
-  <SvelteFlow bind:nodes bind:edges fitView ondragover={onDragOver} ondrop={onDrop}>
-    <Controls />
-    <Background variant={BackgroundVariant.Dots} />
-    <MiniMap />
-  </SvelteFlow>
-  <Sidebar />
-</main>
- 
-<style>
-  main {
-    height: 100vh;
-    display: flex;
-    flex-direction: column-reverse;
-  }
-</style>
+<div style:width="100vw" style:height="100vh">
+<SvelteFlow bind:nodes {nodeTypes} bind:edges {edgeTypes} {defaultEdgeOptions} fitView>
+  <Controls showLock={false} />
+  <svg>
+    <defs>
+      <linearGradient id="edge-gradient">
+        <stop offset="0%" stop-color="#ae53ba" />
+        <stop offset="100%" stop-color="#2a8af6" />
+      </linearGradient>
+      <marker
+        id="edge-circle"
+        viewBox="-5 -5 10 10"
+        refX="0"
+        refY="0"
+        markerUnits="strokeWidth"
+        markerWidth="10"
+        markerHeight="10"
+        orient="auto"
+      >
+        <circle stroke="#2a8af6" stroke-opacity="0.75" r="2" cx="0" cy="0" />
+      </marker>
+    </defs>
+  </svg>
+</SvelteFlow>
+</div>
---
+++ src/lib/flow/TurboNode.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { Handle, Position, type NodeProps } from '@xyflow/svelte';
+  import { FileIcon, CloudIcon } from 'svelte-feather-icons';
+ 
+  import FunctionIcon from './FunctionIcon.svelte';
+ 
+  let { data }: NodeProps = $props();
+</script>
+ 
+<div class="cloud gradient">
+  <div>
+    <CloudIcon size="16" />
+  </div>
+</div>
+<div class="wrapper gradient">
+  <div class="inner">
+    <div class="body">
+      {#if data.icon}
+        <div class="icon">
+          {#if data.icon === 'function'}
+            <FunctionIcon />
+          {:else if data.icon === 'file'}
+            <FileIcon size="16" />
+          {/if}
+        </div>
+      {/if}
+      <div>
+        <div class="title">{data.title}</div>
+        {#if data.subtitle}
+          <div class="subtitle">{data.subtitle}</div>
+        {/if}
+      </div>
+    </div>
+    <Handle type="target" position={Position.Left} />
+    <Handle type="source" position={Position.Right} />
+  </div>
+</div>
